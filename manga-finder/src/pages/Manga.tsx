0c379d9ce00c19f14731545aed70062b5abf775c
JonathanLadwig <jonladwig98@gmail.com>
2023-03-14 11:13:40 +0200

    feat: :construction: Added Beginnings of MangaPage code

--- manga-finder/src/pages/Manga.tsx
+++ manga-finder/src/pages/Manga.tsx
@@ -1,5 +1,30 @@
 import MangaPageInfo from "../components/MangaPageInfo";
 import NavBar from "../components/Navbar";
+import { IManga } from "../models/IManga";
+
+const manga: IManga = {
+  id: "",
+  title: "",
+  altTitles: [],
+  description: undefined,
+  isLocked: false,
+  links: [],
+  originalLanguage: "",
+  lastVolume: "",
+  lastChapter: "",
+  publicationDemographic: "",
+  status: "",
+  year: 0,
+  contentRating: "",
+  tags: [],
+  state: "",
+  chapterNumbersResetOnNewVolume: false,
+  createdAt: "",
+  updatedAt: "",
+  version: 0,
+  availableTranslatedLanguages: [],
+  latestUploadedChapter: "",
+};
 
 function Manga() {
   return (
